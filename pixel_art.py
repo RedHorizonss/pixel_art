c9e16e3447a203aef8e9bd49375bdc6afe3642c9
Marco Barnfield <mbarnfield63@gmail.com>
2023-11-06 11:10:21 +0000

    Restored OG bee & added barbee

--- pixel_art.py
+++ pixel_art.py
@@ -6,21 +6,44 @@ def define_bee():
 
     # define some colours
     black = [0, 0, 0]
-    yellow = [0.7, 0.1, 0.9]
-    grey = [0, 0.85, 0.65]
+    yellow = [1.0, 0.85, 0]
+    grey = [0.65] * 3
+
+    # specify which pixels are which colour
+    image_mat[7:11, 2] = black
+    image_mat[6:12, 3:5] = black
+    image_mat[6:12, 5:7] = yellow
+    image_mat[6:12, 7:9] = black
+    image_mat[6:12, 9:11] = yellow
+    image_mat[6:12, 11:13] = black
+    image_mat[7:11, 13] = black
+    image_mat[4:6, 5:11] = grey
+    image_mat[3, 6:10] = grey
+
+    return image_mat
+
+def define_bee_pink():
+    import numpy as np
+
+    # make an empty numpy array for storing the image
+    image_mat = np.full((16, 16, 3), 1.0)
+
+    # define some colours
+    grey = [0.65] * 3
     pink = [1,0,1]
+    pink_diff = [1,0.85,1]
 
 
     # specify which pixels are which colour
     image_mat[7:11, 2] = pink
     image_mat[6:12, 3:5] = pink
-    image_mat[6:12, 5:7] = yellow
+    image_mat[6:12, 5:7] = pink_diff
     image_mat[6:12, 7:9] = pink
-    image_mat[6:12, 9:11] = yellow
+    image_mat[6:12, 9:11] = pink_diff
     image_mat[6:12, 11:13] = pink
     image_mat[7:11, 13] = pink
-    image_mat[4:6, 5:11] = pink
-    image_mat[3, 6:10] = pink
+    image_mat[4:6, 5:11] = grey
+    image_mat[3, 6:10] = grey
     
     return image_mat
     
@@ -81,7 +104,10 @@ def plot_image(image):
     plt.imshow(image)
     plt.show()
     
+
 bee = define_bee()
 plot_image(bee)
+barbee = define_bee_pink()
+plot_image(barbee)
 butterfly = define_butterfly()
 plot_image(butterfly)
