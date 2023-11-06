5a78a413eaf010d786f6b6bf476ba414a6f1eacd
Marco Barnfield <mbarnfield63@gmail.com>
2023-11-06 10:20:07 +0000

    Proper pink

--- pixel_art.py
+++ pixel_art.py
@@ -5,18 +5,18 @@ def define_bee():
     image_mat = np.full((16, 16, 3), 1.0)
 
     # define some colours
-    black = [(1/255), 0, (1/255)]
-    yellow = [(1/255), 0.85, (1/255)]
+    pink = [1, 0, 1]
+    different_pink = [1, 0.85, 1]
     grey = [0.65] * 3
 
     # specify which pixels are which colour
-    image_mat[7:11, 2] = black
-    image_mat[6:12, 3:5] = black
-    image_mat[6:12, 5:7] = yellow
-    image_mat[6:12, 7:9] = black
-    image_mat[6:12, 9:11] = yellow
-    image_mat[6:12, 11:13] = black
-    image_mat[7:11, 13] = black
+    image_mat[7:11, 2] = pink
+    image_mat[6:12, 3:5] = pink
+    image_mat[6:12, 5:7] = different_pink
+    image_mat[6:12, 7:9] = pink
+    image_mat[6:12, 9:11] = different_pink
+    image_mat[6:12, 11:13] = pink
+    image_mat[7:11, 13] = pink
     image_mat[4:6, 5:11] = grey
     image_mat[3, 6:10] = grey
     
